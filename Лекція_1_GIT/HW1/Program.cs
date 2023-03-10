297dc7192387245379795522b7f793e394740bce
PohribniakDaryna <dpogribnyak@ukr.net>
2023-03-11 01:16:39 +0200

    add app, which counts numbers sum from 1 to 100

--- Лекція_1_GIT/HW1/Program.cs
+++ Лекція_1_GIT/HW1/Program.cs
@@ -4,7 +4,14 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int firstNumber = 1;
+            int lastNumber = 100;
+            int sum =0;
+            for (int i = firstNumber; i <= lastNumber; i++)
+            {
+                sum+=i;
+            }
+            Console.WriteLine($"Sum of numbers from {firstNumber} to {lastNumber} is {sum}.");
         }
     }
 }
