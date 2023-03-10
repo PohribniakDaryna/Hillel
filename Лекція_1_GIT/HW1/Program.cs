6955d3fd46d88a514ff9ca3457123c06878f8a0f
PohribniakDaryna <dpogribnyak@ukr.net>
2023-03-11 01:24:38 +0200

    add logic  and  explanation  in comments

--- Лекція_1_GIT/HW1/Program.cs
+++ Лекція_1_GIT/HW1/Program.cs
@@ -4,12 +4,13 @@
     {
         public static void Main(string[] args)
         {
+            //Task 1. Create app for counting numbers sum from 1 to 100.
             int firstNumber = 1;
             int lastNumber = 100;
-            int sum =0;
+            int sum = 0;
             for (int i = firstNumber; i <= lastNumber; i++)
             {
-                sum+=i;
+                sum += i;
             }
             Console.WriteLine($"Sum of numbers from {firstNumber} to {lastNumber} is {sum}.");
         }
